f9ea6480324d0040f2d701aabe7dc0c241676c00
Jeremy Bass <jeremybass@cableone.net>
2014-01-11 11:50:39 -0800

    openssl for spdy

--- provision/salt/config/nginx/compiler.sh
+++ provision/salt/config/nginx/compiler.sh
@@ -14,15 +14,19 @@ ini(){
     ln -sf nginx-$nginxVersion nginx
     
     cd /src/nginx
+
+    # Fetch openssl
+    wget -N http://www.openssl.org/source/openssl-1.0.1e.tar.gz 2>/dev/null
+    tar -xzf openssl-1.0.1e.tar.gz >/dev/null
     
+
     #mkdir /tmp/nginx-modules
     #cd /tmp/nginx-modules
     #wget https://github.com/agentzh/headers-more-nginx-module/archive/v0.19.tar.gz
     #tar -xzvf v0.19.tar.gz 
+
     
     ./configure \
---user=www-data \
---group=www-data \
 --prefix=/etc/nginx \
 --sbin-path=/usr/sbin/nginx \
 --conf-path=/etc/nginx/nginx.conf \
@@ -30,6 +34,7 @@ ini(){
 --lock-path=/var/run/nginx.lock \
 --error-log-path=/var/log/nginx/error.log \
 --http-log-path=/var/log/nginx/access.log \
+--http-proxy-temp-path=/var/lib/nginx/proxy \
 --with-http_auth_request_module \
 --with-http_sub_module \
 --with-http_mp4_module \
@@ -38,12 +43,18 @@ ini(){
 --with-http_gzip_static_module \
 --with-http_stub_status_module \
 --with-http_ssl_module \
---with-http_spdy_module\
+--with-http_sub_module \
+--with-http_xslt_module \
+--with-http_spdy_module \
+--with-ipv6 \
+--with-sha1=/usr/include/openssl \
+--with-md5=/usr/include/openssl \
 --with-pcre \
 --with-file-aio \
 --with-http_realip_module \
 --without-http_scgi_module \
 --without-http_uwsgi_module \
+--with-openssl='openssl-1.0.1e' \ # wget http://www.openssl.org/source/openssl-1.0.1e.tar.gz
 --with-ipv6 >/dev/null
     make >/dev/null
     make install >/dev/null

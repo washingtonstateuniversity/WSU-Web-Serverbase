f801e464ff2bc48e00701ec84ae380ece0b6c2ad
Jeremy Bass <jeremybass@cableone.net>
2014-02-07 14:33:53 -0800

    typo on end if

--- bootstrap.sh
+++ bootstrap.sh
@@ -126,7 +126,7 @@ init_modgit(){
         curl https://raw.github.com/jeremyBass/modgit/master/modgit > /src/deployment/modgit
         ln -s /src/deployment/modgit /usr/local/bin/modgit
         chmod a=r+w+x /usr/local/bin/modgit
-    if
+    fi
     cd /var/app
     modgit init
 }

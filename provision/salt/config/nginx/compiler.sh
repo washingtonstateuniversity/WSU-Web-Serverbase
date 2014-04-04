9750e42717cf3eebc5531a99250e2c2ab46bc472
Jeremy Bass <jeremybass@cableone.net>
2014-04-04 16:35:22 -0700

    adds flv support, just in case

--- provision/salt/config/nginx/compiler.sh
+++ provision/salt/config/nginx/compiler.sh
@@ -60,6 +60,7 @@ ini(){
 --with-http_auth_request_module \
 --with-http_sub_module \
 --with-http_mp4_module \
+--with-http_flv_module \
 --with-http_addition_module \
 --with-http_dav_module \
 --with-http_gzip_static_module \

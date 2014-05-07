a707bd73cad389281baa928c76eeb1da0065c4a9
jeremyBass <jeremybass@cableone.net>
2014-05-07 09:26:16 -0700

    the global data obj is alway empty

--- server-bootstrap.sh
+++ server-bootstrap.sh
@@ -263,8 +263,8 @@ init_provision_settings(){
 				read -p ">>" answer </dev/tty
 			if [ -f "${provisionpath}${answer}" ]; then
 				echo "The file ${answer} was found, we will begin"
-				done=1
 				load_config_data
+				done=1
 			fi
 		done
 	fi

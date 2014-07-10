18f3097cea2f59ee6825b40e1c55c9063e62ac55
jeremyBass <jeremybass@cableone.net>
2014-07-10 09:12:50 -0700

    starts the rules for iptables to be as restrictive as possible
    by default get drop everything

--- provision/salt/config/iptables/compiler.sh
+++ provision/salt/config/iptables/compiler.sh
@@ -5,6 +5,55 @@
 #then return message only it it's a fail
 ini(){
     cd /src
+	
+	
+	
+	
+	
+	
+	
+	
+# Iptables configuration script
+
+# Flush all current rules from iptables
+/sbin/iptables -F
+
+# Loopback address
+/sbin/iptables -A INPUT -i lo -j ACCEPT
+
+# Allowed any established connections
+/sbin/iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+# Allow FTP and SSH from specific IPs
+#/sbin/iptables -A INPUT -s 10.0.2.0/24 -p tcp -m state --state NEW -m multiport --dports 21,22 -j ACCEPT
+
+# Allow pings from monitoring server
+/sbin/iptables -A INPUT -s 1.1.1.1 -p icmp -m icmp --icmp-type any -j ACCEPT
+
+# Allow web server access from anywhere
+/sbin/iptables -A INPUT -p tcp -m multiport --dports 80,443 -j ACCEPT
+
+# Drop rules to prevent them from entering the logs
+/sbin/iptables -A INPUT -p tcp -m multiport --dports 135,137,138 -j DROP
+/sbin/iptables -A INPUT -p udp -m multiport --dports 135,137,138 -j DROP
+/sbin/iptables -A INPUT -p all -d 255.255.255.255 -j DROP
+
+# Log dropped traffic
+/sbin/iptables -A INPUT -j LOG -m limit --limit 10/m --log-level 4 --log-prefix "Dropped Traffic: "
+
+# Set default policies for INPUT, FORWARD and OUTPUT chains
+/sbin/iptables -P INPUT DROP
+/sbin/iptables -P FORWARD DROP
+/sbin/iptables -P OUTPUT ACCEPT
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	echo "result=True changed=True comment='$resulting'"
 }
 

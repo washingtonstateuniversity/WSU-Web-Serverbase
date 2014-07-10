6abcdf87349a497ce1d24252fea74129e0a035e6
jeremyBass <jeremybass@cableone.net>
2014-07-10 09:10:40 -0700

    start the compiler for iptables setup

---
+++ provision/salt/config/iptables/compiler.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+#set the compiler to be quite
+#then return message only it it's a fail
+ini(){
+    cd /src
+	echo "result=True changed=True comment='$resulting'"
+}
+
+LOGOUTPUT=$(ini)

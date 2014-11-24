55dfbfe53b1ce3ee88c9badcb376dac975169510
Jeremy Bass <jeremybass@cableone.net>
2014-11-24 03:59:26 -0800

    test the markdown

--- builder/tasks/build_site.js
+++ builder/tasks/build_site.js
@@ -12,8 +12,26 @@ module.exports = function(grunt) {
 		var nunjucks = require('nunjucks'),
 			markdown = require('nunjucks-markdown');
 		var env = nunjucks.configure('templates');
-			markdown.register(env);
-		
+		var marked = require('marked');
+			// Async highlighting with pygmentize-bundled
+			marked.setOptions({
+			  highlight: function (code, lang, callback) {
+				require('pygmentize-bundled')({ lang: lang, format: 'html' }, code, function (err, result) {
+				  callback(err, result.toString());
+				});
+			  }
+			});
+			markdown.register(env,{
+				renderer: new marked.Renderer(),
+				gfm: true,
+				tables: true,
+				breaks: false,
+				pendantic: false,
+				sanitize: false,
+				smartLists: true,
+				smartypants: false
+			});
+
 		
 		
 		var fs = require('fs');
@@ -46,7 +64,12 @@ module.exports = function(grunt) {
 				//build nav
 				var tmpobj={};
 				var root = sitemap.pages[page_key].nav_root.replace(new RegExp("[\/]+$", "g"), "");
-				var linkTitle= sitemap.pages[page_key].title;
+				
+				var linkTitle = sitemap.pages[page_key].title;
+				if(typeof sitemap.pages[page_key].nav_title !== "undefined" ){
+					linkTitle = sitemap.pages[page_key].nav_title;
+				}
+
 				if(typeof sitemap.pages[page_key].nav_link !== "undefined" ){
 					tmpobj[linkTitle]=sitemap.pages[page_key].nav_link;
 				}else{
@@ -57,6 +80,9 @@ module.exports = function(grunt) {
 					var navarray = {};
 					
 					var mainlink= sitemap.pages[page_key].title;
+					if(typeof sitemap.pages[page_key].nav_title !== "undefined" ){
+						mainlink = sitemap.pages[page_key].nav_title;
+					}
 					navarray[mainlink] = r;
 					for (var link in sitemap.pages[page_key].child_nav){
 						var url = link;
